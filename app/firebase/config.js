357574590d41b09db1488fe3817f0b43a1b93d9d
Mohammed Sahal <118444006+mohammmmmmmmmmed@users.noreply.github.com>
2024-04-02 00:49:15 +0530

    Update config.js

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -3,14 +3,21 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
+
 const firebaseConfig = {
-  NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyCgBKV9Pyn2qTPWZPFuMrUDauLSnE5FgmE
-NEXT_PUBLIC_AUTH_DOMAIN=linkfolio-12e11.firebaseapp.com
-NEXT_PUBLIC_PROJECT_ID=linkfolio-12e11
-NEXT_PUBLIC_STORAGE_BUCKET=linkfolio-12e11.appspot.com
-NEXT_PUBLIC_APP_ID=1:913257243047:web:76ab22bc8522c604315682
-NEXT_PUBLIC_MEASUREMENT_ID=  // This one is omitted as it's not provided in your input
-MESSAGING_SENDER_ID=913257243047
+  apiKey: "AIzaSyCgBKV9Pyn2qTPWZPFuMrUDauLSnE5FgmE",
+
+  authDomain: "linkfolio-12e11.firebaseapp.com",
+
+  databaseURL: "https://linkfolio-12e11-default-rtdb.firebaseio.com",
+
+  projectId: "linkfolio-12e11",
+
+  storageBucket: "linkfolio-12e11.appspot.com",
+
+  messagingSenderId: "913257243047",
+
+  appId: "1:913257243047:web:76ab22bc8522c604315682"
 
 };
 
